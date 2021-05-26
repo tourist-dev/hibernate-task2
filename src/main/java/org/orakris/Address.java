98e3d596bb4c4ca83371ccd46509154ff7e170f5
tourist-dev <shaik.sameer@orakris.com>
2021-05-26 13:09:18 +0530

    Added Address.java & Student.java

---
+++ src/main/java/org/orakris/Address.java
@@ -0,0 +1,84 @@
+package org.orakris;
+
+import javax.persistence.*;
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String addressline, city, state, country;
+    private int pincode;
+
+    @OneToOne(targetEntity = Student.class)
+    private Student student;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddressline() {
+        return addressline;
+    }
+
+    public void setAddressline(String addressline) {
+        this.addressline = addressline;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(int pincode) {
+        this.pincode = pincode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", addressline='" + addressline + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", pincode=" + pincode +
+                ", student=" + student +
+                '}';
+    }
+}
---
+++ src/main/java/org/orakris/Student.java
@@ -0,0 +1,47 @@
+package org.orakris;
+
+import javax.persistence.*;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @PrimaryKeyJoinColumn
+    private int id;
+    private String name;
+    @OneToOne(targetEntity = Address.class, cascade = CascadeType.ALL)
+    private Address address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
